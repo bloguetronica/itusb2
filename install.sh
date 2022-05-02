1dad8f24af37e843dd4f4cc46d82b296aafef077
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2022-05-02 20:32:39 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt-get -qq update
 apt-get -qq install build-essential
 apt-get -qq install libusb-1.0-0-dev
 echo Copying source code files...
-mkdir -p /usr/local/src/itusb2
+mkdir -p /usr/local/src/itusb2/man
 cp -f src/cp2130.cpp /usr/local/src/itusb2/.
 cp -f src/cp2130.h /usr/local/src/itusb2/.
 cp -f src/error.cpp /usr/local/src/itusb2/.
@@ -29,23 +29,21 @@ cp -f src/LGPL.txt /usr/local/src/itusb2/.
 cp -f src/libusb-extra.c /usr/local/src/itusb2/.
 cp -f src/libusb-extra.h /usr/local/src/itusb2/.
 cp -f src/Makefile /usr/local/src/itusb2/.
+cp -f src/man/itusb2-attach.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-detach.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-enum.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-info.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-list.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-lockotp.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-reset.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-status.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-udoff.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-udon.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-upoff.1 /usr/local/src/itusb2/man/.
+cp -f src/man/itusb2-upon.1 /usr/local/src/itusb2/man/.
 cp -f src/README.txt /usr/local/src/itusb2/.
-echo Building and installing binaries...
-make -C /usr/local/src/itusb2 all install clean
-echo Installing man pages...
-mkdir -p /usr/local/share/man/man1
-cp -f man/itusb2-attach.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-detach.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-enum.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-info.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-list.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-lockotp.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-reset.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-status.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-udoff.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-udon.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-upoff.1.gz /usr/local/share/man/man1/.
-cp -f man/itusb2-upon.1.gz /usr/local/share/man/man1/.
+echo Building and installing binaries and man pages...
+make -C /usr/local/src/itusb2 install clean
 echo Applying configurations...
 cat > /etc/udev/rules.d/70-bgtn-itusb2.rules << EOF
 SUBSYSTEM=="usb", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="8cdf", MODE="0666"
--- uninstall.sh
+++ uninstall.sh
@@ -3,21 +3,7 @@
 echo Rolling back configurations...
 rm -f /etc/udev/rules.d/70-bgtn-itusb2.rules
 service udev restart
-echo Removing man pages...
-rm -f /usr/local/share/man/man1/itusb2-attach.1.gz
-rm -f /usr/local/share/man/man1/itusb2-detach.1.gz
-rm -f /usr/local/share/man/man1/itusb2-enum.1.gz
-rm -f /usr/local/share/man/man1/itusb2-info.1.gz
-rm -f /usr/local/share/man/man1/itusb2-list.1.gz
-rm -f /usr/local/share/man/man1/itusb2-lockotp.1.gz
-rm -f /usr/local/share/man/man1/itusb2-reset.1.gz
-rm -f /usr/local/share/man/man1/itusb2-status.1.gz
-rm -f /usr/local/share/man/man1/itusb2-udoff.1.gz
-rm -f /usr/local/share/man/man1/itusb2-udon.1.gz
-rm -f /usr/local/share/man/man1/itusb2-upoff.1.gz
-rm -f /usr/local/share/man/man1/itusb2-upon.1.gz
-rmdir --ignore-fail-on-non-empty /usr/local/share/man/man1
-echo Removing binaries...
+echo Removing binaries and man pages...
 make -C /usr/local/src/itusb2 uninstall
 echo Removing source code files...
 rm -rf /usr/local/src/itusb2
